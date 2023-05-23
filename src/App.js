8c3531f50fa91214960ec3bb3dc1257645153b0c
sumitdevda2001 <sumitdevda2001@gmail.com>
2023-05-23 14:09:58 +0530

    home page member add

--- src/App.js
+++ src/App.js
@@ -12,37 +12,106 @@ const App = () => {
     <div className="App">
       <Navbar />
       
-      <h1 className="title">{MEMBERS_DATA[0].title}</h1>
+      <h1 className="title0">{MEMBERS_DATA[0].title}</h1>
       <div className="member-container">
       {MEMBERS_DATA[0].members.map((member) => (
+        <div className="card">
       <div  key={member.id}>
       <div className="box">
+      <div className="image">
       <img src={member.image} alt={`${member.first_name} ${member.last_name}`} />
+      </div>
       <div className="text_container">
-      <p>{member.Department}</p>
-        <h2>{`${member.first_name} ${member.last_name}`}</h2>
+      <p className="text-depart">{member.Department}</p>
+        <h2 className="text-name">{`${member.first_name} ${member.last_name}`}</h2>
         </div>
         </div>
+      </div>
       </div>
       ))}
+      
       </div>
+      
 
 
-      <h1 className="title">{MEMBERS_DATA[1].title}</h1>
+
+      <h1 className="title1">{MEMBERS_DATA[1].title}</h1>
       <div className="member-container">
-      {MEMBERS_DATA[1].members.map((member) => (
+      {MEMBERS_DATA[1].members.slice(0, 5).map((member) => (
+        <div className="card">
       <div key={member.id}>
       <div className="box">
+      <div className="image">
       <img src={member.image} alt={`${member.first_name} ${member.last_name}`} />
+      </div>
       <div className="text_container">
-      <p>{member.Department}</p>
-        <h2>{`${member.first_name} ${member.last_name}`}</h2>
+      <p className="text-depart">{member.Department}</p>
+        <h2 className="text-name">{`${member.first_name} ${member.last_name}`}</h2>
         </div>
         </div>
+      </div>
+      </div>
+      ))}
+      </div> 
+
+
+      <div className="space" ></div>
+      <div className="member-container">
+      {MEMBERS_DATA[1].members.slice(5, 10).map((member) => (
+        <div className="card">
+      <div key={member.id}>
+      <div className="box">
+      <div className="image">
+      <img src={member.image} alt={`${member.first_name} ${member.last_name}`} />
+      </div>
+      <div className="text_container">
+      <p className="text-depart">{member.Department}</p>
+        <h2 className="text-name">{`${member.first_name} ${member.last_name}`}</h2>
+        </div>
+        </div>
+      </div>
       </div>
       ))}
       </div>
 
+      <div className="space" ></div>
+      <div className="member-container">
+      {MEMBERS_DATA[1].members.slice(10, 15).map((member) => (
+        <div className="card">
+      <div key={member.id}>
+      <div className="box">
+      <div className="image">
+      <img src={member.image} alt={`${member.first_name} ${member.last_name}`} />
+      </div>
+      <div className="text_container">
+      <p className="text-depart">{member.Department}</p>
+        <h2 className="text-name">{`${member.first_name} ${member.last_name}`}</h2>
+        </div>
+        </div>
+      </div>
+      </div>
+      ))}
+      </div>
+
+      <div className="space" ></div>
+      <div className="member-container-last">
+      {MEMBERS_DATA[1].members.slice(15, 20).map((member) => (
+        <div className="card">
+      <div key={member.id}>
+      <div className="box">
+      <div className="image">
+      <img src={member.image} alt={`${member.first_name} ${member.last_name}`} />
+      </div>
+      <div className="text_container">
+      <p className="text-depart">{member.Department}</p>
+        <h2 className="text-name">{`${member.first_name} ${member.last_name}`}</h2>
+        </div>
+        </div>
+      </div>
+      </div>
+      ))}
+      </div>
+      <div className="space" ></div>
       
     </div>
   );
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,41 +1,44 @@
 import React from "react";
 import "./navbar.css";
-import { NotificationsNone,Language,Settings } from "@material-ui/icons";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
 // import {ReactComponent as Imlogo } from "../../src/assets/im-logo .svg"
-import {ReactComponent as Imlogo} from "../../assets/im-logo .svg"
+import { ReactComponent as Imlogo } from "../../assets/im-logo .svg";
+import { useState } from "react";
 
 export default function Navbar() {
   return (
     <div className="topbar">
       <div className="topbarWrapper">
-      <div className="topbarLeft">
-      <Imlogo className="logo" />
+        <div className="topbarLeft">
+          <Imlogo className="logo" />
         </div>
         <div className="topRight">
-        <div className="topbarIconContainer">
-        
+          <div className="topbarIconContainer">
             <span className="search">Search</span>&nbsp;&nbsp;&nbsp;
             <span className="Home">Home</span>&nbsp;&nbsp;&nbsp;
             <span className="about">About</span>&nbsp;&nbsp;&nbsp;
             <span className="Contactus">Contact Us</span>&nbsp;&nbsp;&nbsp;
             <span className="Signup">Sign Up</span>&nbsp;&nbsp;&nbsp;
             <span className="login">Login</span>
-
-            </div>
+          </div>
           <div className="topbarIconContainer">
             <NotificationsNone />
             <span className="topIconBadge">2</span>
-            </div>
-            <div className="topbarIconContainer">
+          </div>
+          <div className="topbarIconContainer">
             <Language />
             <span className="topIconBadge">10</span>
-            </div>
-            <div className="topbarIconContainer">
-            <Settings />
-            </div>
-            <img src="https://cdn.pixabay.com/photo/2018/01/09/15/18/ornament-3071827_640.png" alt="" className="topAvatar" />
           </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="https://cdn.pixabay.com/photo/2018/01/09/15/18/ornament-3071827_640.png"
+            alt=""
+            className="topAvatar"
+          />
         </div>
       </div>
+    </div>
   );
 }
